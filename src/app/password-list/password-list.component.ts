81fb466af65dd7fdfe3e198a1e7f87e648f3e2b0
Anas Bensaid <bensaidanass9@gmail.com>
2023-07-16 12:34:20 +0100

    Adding Password List

--- src/app/password-list/password-list.component.ts
+++ src/app/password-list/password-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PasswordManagerService } from '../services/password-manager.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-password-list',
@@ -7,4 +10,71 @@ import { Component } from '@angular/core';
 })
 export class PasswordListComponent {
 
+  siteId!: string;
+  siteName!: string;
+  siteUrl!: string;
+  siteImgUrl!: string;
+
+  passwordList!: Observable<Array<any>>;
+
+  email!: string;
+  username!: string;
+  password!: string;
+  passowrdId!: string;
+
+  formState = "Add New"
+
+  constructor(private route: ActivatedRoute, private passowrdService: PasswordManagerService) {
+    this.route.queryParams.subscribe((val: any) => {
+      this.siteId = val.id;
+      this.siteName = val.siteName;
+      this.siteUrl = val.siteUrl;
+      this.siteImgUrl = val.siteImgUrl;
+    })
+
+    this.loadPasswords();
+  }
+
+  onSubmit(values: object) {
+    if (this.formState === "Add New") {
+      this.passowrdService.addPassword(values, this.siteId)
+      .then(() => {
+        console.log("Password Save Successfully")
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    } else if (this.formState === "Edit") {
+      this.passowrdService.updatePassword(this.siteId, this.passowrdId, values)
+      .then(() => {
+        console.log("Data Updated")
+        this.resetForm();
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+    
+  }
+
+  loadPasswords() {
+    this.passwordList = this.passowrdService.loadPasswords(this.siteId)
+  }
+
+  editPassword(email: string, username: string, password: string, passwordId: string) {
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    this.passowrdId = passwordId
+
+    this.formState = "Edit"
+  }
+
+  resetForm() {
+    this.email = '';
+    this.username = '';
+    this.password = '';
+    this.passowrdId = '';
+    this.formState = "Add New";
+  }
 }
